90d6073563f0196edd32aa4dbceb9ca712368cfa
Chris Milios <cmilios@hotmail.com>
2021-04-29 17:13:12 +0300

    final

--- mapper.py
+++ mapper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 # input comes from STDIN (standard input)
-list = []
+# list = []
 
 for line in sys.stdin:
     # remove leading and trailing whitespace
@@ -12,21 +12,22 @@ for line in sys.stdin:
     ip = columns[0]
     date = columns[1]
     time = columns[2][:-3]
-    x = {'Key': ip, "Value": {'Date': date, 'Time': time}}
-    for y in list:
-        if(y['Key'] == x['Key']):
-            if(y['Value']['Date'] == x['Value']['Date']):
-                if(y["Value"]["Time"] == x['Value']['Time']):
-                    pass
-                else:
-                    list.append(x)
-                    print(ip, 1)
-            else:
-                list.append(x)
-                print(ip, 1)
-        else:
-            list.append(x)
-            print(ip, 1)
+    value = date+","+time
+    print(ip, value)
+    # for y in list:
+    #     if(y['Key'] == x['Key']):
+    #         if(y['Value']['Date'] == x['Value']['Date']):
+    #             if(y["Value"]["Time"] == x['Value']['Time']):
+    #                 pass
+    #             else:
+    #                 list.append(x)
+    #                 print(ip, 1)
+    #         else:
+    #             list.append(x)
+    #             print(ip, 1)
+    #     else:
+    #         list.append(x)
+    #         print(ip, 1)
 
     
 
